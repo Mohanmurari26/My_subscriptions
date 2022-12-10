7d3cf3111f84cb5e21e73057c9989fa6ff9a5770
Mohanmurari26 <84223328+Mohanmurari26@users.noreply.github.com>
2022-12-10 11:48:42 +0530

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-monday = {"TOI": 3, "HINDU": 2.5, "ET": 4, "BM": 1.5, "HT": 2}
-tuesday = {"TOI": 3, "HINDU": 2.5, "ET": 4, "BM": 1.5, "HT": 2}
-wednesday = {"TOI": 3, "HINDU": 2.5, "ET": 4, "BM": 1.5, "HT": 2}
-thursday = {"TOI": 3, "HINDU": 2.5, "ET": 4, "BM": 1.5, "HT": 2}
-friday = {"TOI": 3, "HINDU": 2.5, "ET": 4, "BM": 1.5, "HT": 2}
-saturday = {"TOI": 5, "HINDU": 4, "ET": 4, "BM": 1.5, "HT": 4}
-sunday = {"TOI": 6, "HINDU": 4, "ET": 10, "BM": 1.5, "HT": 4}
-week_list = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
-
-weekly_budget = int(input())
-sum = 0
-for i in week_list:
-    result = set()
-    for j in list(i.keys()):
-            sum += i[j]
-
-if int(sum) >= weekly_budget:
-    print("time is not sufficient to solve this!!!")
-
+budget = int(input())
+A = [26,20,34,10.5,18]
+lst= ['TOI','Hindu','ET','BM','HT']
+for i in range(len(A)):
+    for j in range(i,len(A)):
+        if (A[i]+A[j] < budget):
+            if list[i] == lst[j]:
+                continue
+            else:
+                result = {lst[i],lst[j]}
+            if len(result) == 1 :
+                continue
+            else:
+                print(result)
 
